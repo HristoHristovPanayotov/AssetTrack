cda1ccf0aef680bcd4e2b53627ba960953827ea6
HristoHristovPanayotov <emailformyself123123@gmail.com>
2026-06-05 22:11:35 +0300

    Initial commit

---
+++ AssetTrackWebProj/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AssetTrackWebProj.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
