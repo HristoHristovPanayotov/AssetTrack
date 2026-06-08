3d5ed4c3c0452d9c76766c8d2fc366a008c047a7
HristoHristovPanayotov <emailformyself123123@gmail.com>
2026-06-08 20:28:16 +0300

    finished adding the models in the services layer

---
+++ AssetTrackWebProj.Services/Models/Common/UserOption.cs
@@ -0,0 +1,8 @@
+namespace AssetTrack.Services.Models.Common
+{
+    public class UserOption
+    {
+        public string Id { get; set; } = null!;
+        public string DisplayName { get; set; } = null!;
+    }
+}
